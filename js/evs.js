7f6527b52639735f3593e90148aa8b800146d2c4
Husein <61166455+Hussein115@users.noreply.github.com>
2023-02-03 19:26:21 +0530

    Add files via upload

---
+++ js/evs.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let evs = [
+    {
+    numb: 1,
+    question: "Rhino found in kaziranga ?",
+    answer: "One horned",
+    options: [
+      "Two horned",
+      "One horned",
+      "Three horned ",
+      "Hornless"
+    ]
+  },
+    {
+    numb: 2,
+    question: "An eco system consist of ________ ?",
+    answer: "Biotic and Abiotic factors",
+    options: [
+      "Population",
+      "Abiotic community",
+      "Animal population and no-living elements",
+      "Biotic and Abiotic factors"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The word environment is derive from ?",
+    answer: "France",
+    options: [
+      "India",
+      "France",
+      "Germany",
+      "English"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Earth day is celebrated every year on ?",
+    answer: "22nd April",
+    options: [
+      "26th October",
+      "25th December",
+      "22nd April",
+      "31st January"
+    ]
+  },
+    {
+    numb: 5,
+    question: "In India” National Food security Act” was enhanced in the year ?",
+    answer: "2013",
+    options: [
+      "2013",
+      "2011",
+      "2009",
+      "1997"
+    ]
+  },
+     {
+    numb: 6,
+    question: "Noise pollution is measured in ?",
+    answer: "Decibel unit",
+    options: [
+      "Gram",
+      "Dobson unit",
+      "Hertz",
+      "Decibel unit"
+    ]
+  },
+     {
+    numb: 7,
+    question: "Green house gas is _______ ?",
+    answer: "Chlorofluorocarbon",
+    options: [
+      "Oxygen",
+      "Chlorofluorocarbon",
+      "Chlorine",
+      "Chlorobenzene"
+    ]
+  },
+     {
+    numb: 8,
+    question: "The term ‘ecosystem’ was termed by ?",
+    answer: "A.G Tansley",
+    options: [
+      "Ernest Hackle",
+      "Aristotle",
+      "Hooker",
+      "A.G Tansley"
+    ]
+  },
+     {
+    numb: 9,
+    question: "Which of the following is situ conservation ?",
+    answer: "National park",
+    options: [
+      "Aquarium",
+      "National park",
+      "Zoo",
+      "Botanical Garden"
+    ]
+  },
+     {
+    numb: 10,
+    question: "The chief source of energy in environment is ?",
+    answer: "Sun",
+    options: [
+      "Sun",
+      "Human",
+      "Animals",
+      "Crude Oil"
+    ]
+  },
+];
---
+++ js/itc.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let itc = [
+    {
+    numb: 1,
+    question: "What is the built-in permanent memory in a computer called ?",
+    answer: "ROM",
+    options: [
+      "ROM",
+      "RAM",
+      "CD-ROM",
+      "CPU"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The lowest form of computer language is called ?",
+    answer: "Machine Language",
+    options: [
+      "BASIC",
+      "FORTRAN",
+      "Machine Language",
+      "COBOL"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following is not an output device ?",
+    answer: "Mouse",
+    options: [
+      "Speaker",
+      "Printer",
+      "Monitor",
+      "Mouse"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which one of the following is the brain of computer ?",
+    answer: "CPU",
+    options: [
+      "CPU",
+      "Circuit board",
+      "ALU",
+      "Memory"
+    ]
+  },
+    {
+    numb: 5,
+    question: "A collection of 8 bit is called……… ?",
+    answer: "Byte",
+    options: [
+      "Nibble",
+      "Byte",
+      "Word",
+      "Record"
+    ]
+  },
+     {
+    numb: 6,
+    question: "Who is known as the father of the modern comuter ?",
+    answer: "Charles Babbage",
+    options: [
+      "John Atanasoff",
+      "Herman Hollerith",
+      "Blaise Pascal",
+      "Charles Babbage"
+    ]
+  },
+     {
+    numb: 7,
+    question: "A worksheet is a ……………………… ?",
+    answer: "Combination of rows and columns ",
+    options: [
+      "Combination of rows and columns ",
+      "Collection of workbooks",
+      "Processing software",
+      "None of these above"
+    ]
+  },
+     {
+    numb: 8,
+    question: "Second generation of computer were manufactured using which technology ?",
+    answer: "Transistors",
+    options: [
+      "Vacuum tube",
+      "Transistors",
+      "ICs",
+      "None of these"
+    ]
+  },
+     {
+    numb: 9,
+    question: "Storage of  1kb means the following number of bytes :",
+    answer: "1024",
+    options: [
+      "1000",
+      "1042",
+      "1012",
+      "1024"
+    ]
+  },
+     {
+    numb: 10,
+    question: "Which of the following is not a computer hardware ?",
+    answer: "Assembler",
+    options: [
+      "Hard disk",
+      "Printer",
+      "Assembler",
+      "CPU"
+    ]
+  },
+];
